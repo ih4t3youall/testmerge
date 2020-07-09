7ad8e63ab22bbcd42e27d857bff748a18e697248
Juan Martin Lequerica <juan.martin.lequerica@jmlequerica-ch--ul>
2020-07-09 07:26:51 -0500

    adding more cuentas

--- Main.java
+++ Main.java
@@ -7,12 +7,14 @@ public class Main {
   }
 
   public Main(){
-  
     int a = 2;
     int b = 3;
     int result = sum(a,b);
     System.out.println(result);
-  
+    result = resta(a,b);
+    System.out.println(result);
+    result = mult(a,b);
+    System.out.println(result);
   }
 
   private int sum(int a, int b){
@@ -23,6 +25,22 @@ public class Main {
     return result;
   
   }
+
+  private int resta(int a, intb){
+    int vara = a;
+    int varb = b;
+    int result = vara+varb;
+    return result;
+  }
+
+
+  private int mult(int a, int b){
+    int vara = a;
+    int varb = b;
+    int result = vara + varb;
+    return result;
+  }
+
 
 
 }
